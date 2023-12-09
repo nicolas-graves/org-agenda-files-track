64de4846dbd65b0c5357e9d46cc7bca25a6d7b1d
Nicolas Graves <ngraves@ngraves.fr>
2023-12-09 15:53:57 +0100

    Expand commentary.

--- org-dynamic-agenda-ql.el
+++ org-dynamic-agenda-ql.el
@@ -23,7 +23,20 @@
 
 ;;; Commentary:
 ;; Fine-track `org-agenda-files' to speed-up `org-ql-views'
-;; See more info here: https://git.sr.ht/~ngraves/org-dynamic-agenda/blob/master/README.org
+
+;; When an agenda buffer is built, Emacs visits each file listed in
+;; `org-agenda-files'.  In case your tasks or events are recorded in
+;; an ever-extending journal and/or roam directories, `org-agenda' can
+;; become sluggish.
+;; This package aims to dynamically update the `org-agenda-files'
+;; variable by appending/deleting a candidate org file when it is
+;; saved.  This limits the number of files to visit when building the
+;; agenda.  The agenda buffer thus builds faster.  Candidate selection
+;; logic is extracted from `org-agenda-custom-commands' and
+;; `org-ql-views'.
+
+;; See more info here:
+;; https://git.sr.ht/~ngraves/org-dynamic-agenda/blob/master/README.org
 
 ;;; Code:
 (require 'org-agenda)
--- org-dynamic-agenda.el
+++ org-dynamic-agenda.el
@@ -23,7 +23,19 @@
 
 ;;; Commentary:
 ;; Fine-track `org-agenda-files' to speed-up `org-agenda'
-;; See more info here: https://git.sr.ht/~ngraves/org-dynamic-agenda/blob/master/README.org
+
+;; When an agenda buffer is built, Emacs visits each file listed in
+;; `org-agenda-files'.  In case your tasks or events are recorded in
+;; an ever-extending journal and/or roam directories, `org-agenda' can
+;; become sluggish.
+;; This package aims to dynamically update the `org-agenda-files'
+;; variable by appending/deleting a candidate org file when it is
+;; saved.  This limits the number of files to visit when building the
+;; agenda.  The agenda buffer thus builds faster.  Candidate selection
+;; is governed by `org-dynamic-agenda-predicate'.
+
+;; See more info here:
+;; https://git.sr.ht/~ngraves/org-dynamic-agenda/blob/master/README.org
 
 ;;; Code:
 (require 'org-agenda)
